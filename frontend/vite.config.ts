3f2c608c4038ce6b48becca3acad6827237fc3f4
visdauas <visdauas@gmail.com>
2023-02-25 18:36:17 +0100

    Fix build issue

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import alias from '@rollup/plugin-alias'
 
 export default defineConfig({
-	plugins: [alias(), sveltekit()],
-	resolve: {
-		alias: {
-		  "@concordium/web-sdk": "concordium-web-sdk-vite",
-		},
-	  },
+	plugins: [sveltekit()],
+	optimizeDeps: {
+		exclude: ['@concordium/web-sdk','concordium/browser-wallet-api-helpers'],
+	},
 });
